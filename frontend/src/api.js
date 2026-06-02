5ed3e944b1cafcf568ddfc2c01c5bee830a50976
Kashish <kashishkiraula2410@gmail.com>
2026-06-02 19:04:21 +0530

    add render url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://applysmartmart-backend.onrender.com";
 
 export async function screenResume(resume, jobDescription) {
   const response = await fetch(`${BASE_URL}/screen`, {
